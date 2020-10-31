3f78ea572b6f99ea026c616266d81f9f0d286015
Andrew Kross <andkross@yandex.ru>
2020-10-31 21:04:56 +0300

    initial commit

---
+++ src/const.ts
@@ -0,0 +1,16 @@
+export const Config = {
+    elementWidth: 35, //px
+    elementRightMargin: 5, //px
+    transitionSpeed: 0.2, //sec
+    switchingDelay: 100, //ms
+    heightMultiplier: 3,
+    maxValue: 100,
+    defaultElementsCount: 10,
+    maxElementsCount: 45,
+}
+
+export const Colors = {
+    elementDefault: `#9755c9`,
+    elementActive: `#c157af`,
+    elementDone: `#ac3469`,
+}
